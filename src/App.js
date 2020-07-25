af027b201bb8cb82749d14ee6ebdb6165776f97f
saiteja-bma <bst634@gmail.com>
2020-07-25 16:46:45 +0530

    theme context

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React, { StrictMode } from "react";
+import React, { StrictMode,useState } from "react";
 import ReactDOM from "react-dom";
 import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
+
 
 const App = () => {
+  const theme = useState("darkblue");
   return (
-    <StrictMode>
-      <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
-    </StrictMode>
+    <ThemeContext.Provider value={theme}>
+      <StrictMode>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </StrictMode>
+    </ThemeContext.Provider>
   );
 };
 
