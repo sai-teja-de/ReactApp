c26e60bcc8d8013fbdc0077b152989d9711f8c1f
saiteja-bma <bst634@gmail.com>
2020-07-25 15:19:52 +0530

    Details route, custom Babel config, add Carousel

--- src/App.js
+++ src/App.js
@@ -4,22 +4,20 @@ import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
-
 const App = () => {
   return (
     <StrictMode>
       <div>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Router>
-            <SearchParams path="/" />
-            <Details path="/details/:id" />
-          </Router>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </StrictMode>
   );
 };
-
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,58 +1,60 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 import Results from "./Results";
 
-const SearchParams =() => {
-    const [location, updateLocation] = useState('Seattle, WA');
-    const [breeds, updateBreeds] = useState([]);
-    const [animal, AnimalDropdown] = useDropdown("Animal",'dog',ANIMALS);
-    const [breed, BreedDropdown, updateBreed] = useDropdown("Breed","",breeds);
-    const [pets, updatePets] = useState([]);
+const SearchParams = () => {
+  const [location, updateLocation] = useState("Seattle, WA");
+  const [breeds, updateBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, updateBreed] = useDropdown("Breed", "", breeds);
+  const [pets, updatePets] = useState([]);
 
-    const requestsPets = async () => {
-        const { animals } = await pet.animals({
-            location,
-            breed,
-            type: animal
-        });
-        updatePets(animals || []);
-    }
+  const requestsPets = async () => {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
+    updatePets(animals || []);
+  };
 
-    useEffect(() => {
-        updateBreeds([]);
-        updateBreed("");
-        pet.breeds(animal).then(({ breeds }) => {
-            const breedStrings = breeds.map(({ name }) => name);
-            updateBreeds(breedStrings);
-        // eslint-disable-next-line no-console
-        },console.error);
-    },[animal,updateBreeds,updateBreed]);
+  useEffect(() => {
+    updateBreeds([]);
+    updateBreed("");
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      updateBreeds(breedStrings);
+      // eslint-disable-next-line no-console
+    }, console.error);
+  }, [animal, updateBreeds, updateBreed]);
 
-    return (
-        <div className="search-params">
-            <h1>{location}</h1>
-            <form onSubmit={e => {
-                e.preventDefault();
-                requestsPets();
-            }}
-            >
-                <label htmlFor="location">
-                    Location
-                    <input id="location" 
-                    value={location} 
-                    placeholder="Location"
-                    onChange={e => updateLocation(e.target.value)}
-                    />
-                </label>
-                < AnimalDropdown />
-                < BreedDropdown />
-                <button>Submit</button>
-            </form>
-            
-            < Results pets={pets} />
-        </div>
-    );
+  return (
+    <div className="search-params">
+      <h1>{location}</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestsPets();
+        }}
+      >
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => updateLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+
+      <Results pets={pets} />
+    </div>
+  );
 };
 
 export default SearchParams;
